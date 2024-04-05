8ae33700df922ea5338711ca90d94ce3206eb5e9
MickMacha <kimmike1100@gmail.com>
2024-04-05 18:36:08 +0300

    added category and review db schemas

---
+++ src/models/category.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name: {type:String, required:True},
+    products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product'}]
+});
+
+const Category = mongoose.model("Category", categorySchema);
+module.exports = Category;
---
+++ src/models/review.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+    product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
+    customer: {type: mongoose.Schema.Types.ObjectId, ref: 'Customer'},
+    rating: {type: Number, required: true},
+    comment: {type: String}
+}
+)
